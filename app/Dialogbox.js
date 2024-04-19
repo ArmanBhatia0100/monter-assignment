b7d0077f9db3bc1a7308bf464040ca25e52f6b96
Arman Bhatia <arman.bhatia.1407@gmail.com>
2024-04-18 23:52:25 -0400

    placed the dummy object into a seprate file for better readabilty.

--- app/Dialogbox.js
+++ app/Dialogbox.js
@@ -1,12 +1,29 @@
+"use client";
 import Header from "./Header";
 import Datasheet from "./Datasheet";
+import Pagination from "./Pagination";
+import reports from "./records";
+import { useState } from "react";
 
 const Dialogbox = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [recordsPerPage, setRecoredsPerPage] = useState(6);
+  const indexOfLastRecord = currentPage * recordsPerPage;
+  const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
+  const currentRecords = reports.slice(indexOfFirstRecord, indexOfLastRecord);
+  const nPages = Math.ceil(reports.length / recordsPerPage);
   return (
-    <div className="dialogbox container max-w-[90vw] mx-auto max-h-[90vh] mt-5 pb-10 rounded-lg shadow-md bg-slate">
+    <div className="dialogbox container max-w-[90vw] mx-auto mt-5 pb-4 rounded-lg shadow-md bg-slate">
       <Header /> {/* This included the title and action btns */}
-      <Datasheet /> {/* Main data imported in the table */}
-      <footer></footer> {/* custom paginator with a row selector */}
+      <Datasheet currentRecords={currentRecords} />{" "}
+      {/* Main data imported in the table */}
+      <hr className="mb-2"></hr>
+      <Pagination
+        nPages={nPages}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />{" "}
+      {/* custom paginator with a row selector */}
     </div>
   );
 };
--- app/Pagination.jsx
+++ app/Pagination.jsx
@@ -3,46 +3,40 @@ import React from 'react'
 
 const Pagination = ({ nPages, currentPage, setCurrentPage }) => {
 
-    const pageNumbers = [...Array(nPages + 1).keys()].slice(1)
-
-    
-
+    const pageNumbers = [...Array(nPages + 1).keys()].slice(1) // [...Array(nPages + 1).keys()].slice(1) generates an array of numbers from 1 to nPages.
     const goToNextPage = () => {
-            if(currentPage !== nPages) setCurrentPage(currentPage + 1)
+            if(currentPage !== nPages) setCurrentPage(currentPage + 1) // checking if the page is last or not if the page is last it does update the current page.
     }
     const goToPrevPage = () => {
-        if(currentPage !== 1) setCurrentPage(currentPage - 1)
+        if(currentPage !== 1) setCurrentPage(currentPage - 1) 
     }
     return (
-        <nav>
+        <nav className=''>
             <ul className='flex justify-around pagination'>
                 <li className="page-item">
-                    <Link className="page-link" 
+                    <Link className="page-link hover:text-red-500" 
                         onClick={goToPrevPage} 
                         href='#'>
-                        
                         Previous
                     </Link>
                 </li>
                 {pageNumbers.map(pgNumber => (
                     <li key={pgNumber} 
-                        className="" >
-
+                        className={currentPage===pgNumber?"text-red-500":""} >
                         <Link onClick={() => setCurrentPage(pgNumber)}  
-                            className='page-link' 
+                            className='page-link hover:text-red-500' 
                             href='#'>
-                            
+                              
                             {pgNumber}
                         </Link>
                     </li>
                 ))}
                 <li className="page-item">
-                    <a className="page-link" 
+                    <Link className="page-link" 
                         onClick={goToNextPage}
                         href='#'>
-                        
                         Next
-                    </a>
+                    </Link>
                 </li>
             </ul>
         </nav>
